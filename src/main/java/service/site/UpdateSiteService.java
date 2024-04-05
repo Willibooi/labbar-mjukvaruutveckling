1b5120a9d3e98ac8f2d8086bed86402606448127
GTSA - Infinity <williamlindahl03@gmail.com>
2024-04-05 23:14:27 +0200

    Second commit

---
+++ src/main/java/service/site/UpdateSiteService.java
@@ -0,0 +1,14 @@
+package service.site;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author GTSA - Infinity
+ */
+public class UpdateSiteService {
+    
+}
